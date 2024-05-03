4e9b35ae7131e5873e8addc3700a973887b35abc
Nakamatetako <contato.naka@hotmail.com>
2024-05-03 01:23:24 -0300

    Novo footer para view

---
+++ src/components/FooterComponentSlots.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div class="w-screen h-screen footer">
+    <div class="flex jackpot bottom-0 w-[500px] h-[600px]">
+      <h1 class="Logo">JACKPOT</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.footer {
+  width: 100%;
+  height: 600px;
+  background-image: url('/src/assets/BackgroundFooter.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+</style>
