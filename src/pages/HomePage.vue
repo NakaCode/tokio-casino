a33aaa5fbf96d274a775bced584dd924edcb6478
Waswass <wasalmeidasilva@gmail.com>
2024-05-03 02:07:57 -0300

    Implementação de card menu

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,13 @@
-<script setup></script>
+<script setup>
+import CardComponent from '@/components/CardComponent.vue';
+</script>
 
 <template>
-  <h2>Home</h2>
+  <main class=" h-full flex justify-center items-center flex-col gap-10">
+
+    <CardComponent title="Abrobinha" icon="./public/Icon1.png" gameIcon="./public/777.svg" active="100" online="200"/>
+    
+    <CardComponent title="Abrobinha" icon="./public/Icon2.png" gameIcon="./public/8ball.svg" active="100" online="200"/>
+
+  </main>
 </template>
